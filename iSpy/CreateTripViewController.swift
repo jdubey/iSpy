a056def7242a5c3b40d6b4288fb96e20dc252b0a
Josh Dubey <jodubey@deloitte.co.uk>
2017-05-09 14:08:50 +0100

    test new location

--- iSpy/CreateTripViewController.swift
+++ iSpy/CreateTripViewController.swift
@@ -18,7 +18,7 @@ class CreateTripViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view.  test commit
     }
 
     override func didReceiveMemoryWarning() {
