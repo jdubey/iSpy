12e79f05a827865fd77f073ae09f44879b76c55e
Josh Dubey <jodubey@deloitte.co.uk>
2017-09-28 11:39:24 +0100

    remove cell expansion and go to trip detail instead

--- iSpy/Map/PlatesTableViewCell.swift
+++ iSpy/Map/PlatesTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 
 protocol PlatesTableViewCellDelegate: class {
-    func plateCellExpansionDidChange(_ cell: PlatesTableViewCell)
+    func didTapShowDetail(_ cell: PlatesTableViewCell)
 }
 
 class PlatesTableViewCell: UITableViewCell {
@@ -18,24 +18,16 @@ class PlatesTableViewCell: UITableViewCell {
     @IBOutlet private weak var nameLabel: UILabel!
     @IBOutlet private weak var plateImageView: UIImageView!
     @IBOutlet private weak var mapView: MKMapView!
-    @IBOutlet weak var expandButton: UIButton!
+    @IBOutlet weak var showTripButton: UIButton!
     @IBOutlet weak var mapViewHeightConstraint: NSLayoutConstraint!
 
     weak var delegate: PlatesTableViewCellDelegate?
-
-    var mapviewDidInit = false
 
     var imageName: String? {
         didSet {
             if imageName != nil {
                 plateImageView.image = UIImage(named:imageName!)
             }
-        }
-    }
-
-    var isExpanded: Bool = false {
-        didSet {
-            adjustCellHeight()
         }
     }
 
@@ -69,56 +61,18 @@ class PlatesTableViewCell: UITableViewCell {
         }
     }
 
-    private func setUpMapView(_ location: Location) {
-        let center = CLLocationCoordinate2DMake(location.latitude, location.longitude)
-        let span = MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0)
-        let region = MKCoordinateRegionMake(center, span)
-
-        mapView.setRegion(region, animated: true)
-
-        let annotation = AnnotationView(frame: CGRect(x: 10, y: 10, width: 10, height: 10), coordinate: center)
-        annotation.backgroundColor = .green
-        mapView.addAnnotation(annotation)
-
-        mapviewDidInit = true
-    }
-
     private func showButton(_ shouldShow: Bool) {
         if shouldShow {
-            expandButton.isHidden = false
-            expandButton.isEnabled = true
+            showTripButton.isHidden = false
+            showTripButton.isEnabled = true
         } else {
-            expandButton.isHidden = true
-            expandButton.isEnabled = false
-        }
-    }
-
-    private func adjustCellHeight() {
-
-        if isExpanded {
-            mapViewHeightConstraint.constant = 250
-        } else {
-            mapViewHeightConstraint.constant = 0
-        }
-
-        updateConstraints()
-    }
-
-    override func updateConstraints() {
-        super.updateConstraints()
-        if let location = location, mapviewDidInit == false {
-            setUpMapView(location)
+            showTripButton.isHidden = true
+            showTripButton.isEnabled = false
         }
     }
 
     // MARK: - Actions
     @IBAction func didTapExpandButton(_ sender: Any) {
 
-        delegate?.plateCellExpansionDidChange(self)
-
-    }
-
-    override func prepareForReuse() {
-        mapviewDidInit = false
     }
 }
